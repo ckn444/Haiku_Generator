d400239e928cf386194524c17ede867efcbacb9b
ckn444 <cknewton@y7mail.com>
2025-10-11 16:55:15 +1000

    added basic javascript for form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-//alert("Hello");
+function createPoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "hello update the text",
+    autoStart: true,
+    cursor: "",
+    delay: 20,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", createPoem);
